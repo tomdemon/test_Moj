9ff1a16903fdd0f8b86a2a2a386abd61cb670a35
Tom <4308@tenbit.pl>
2019-09-08 20:23:09 +0200

    Dodano zbedny komentarz.

--- src/test2/Main.java
+++ src/test2/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         int[] tabela1D = new int[13];
 
+        // komentarz
         for (int i = 12; i >= 0; i--) {
             tabela1D[i] = i+1;
             System.out.println(tabela1D[i]);
