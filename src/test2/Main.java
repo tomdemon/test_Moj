05248e9058137164dbc51de9e00843342c23abbf
Humberd <humberd2@gmail.com>
2019-09-08 20:27:03 +0200

    Zaktualizowalem komentarz

--- src/test2/Main.java
+++ src/test2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // to nie powinno byc tak
-        System.out.println("To powinno byc tak");
+        System.out.println("To powinno byc tak, albo tak");
         int[] tabela1D = new int[13];
 
         // komentarz
