aa4acc6efac10adc51997f7ad6a7678664d4122c
Humberd <humberd2@gmail.com>
2019-10-29 00:00:36 +0100

    Refactored 5_8

--- src/_002/Zadania_5/Zadanie_5_8.java
+++ src/_002/Zadania_5/Zadanie_5_8.java
@@ -13,29 +13,37 @@ import java.util.StringTokenizer;
 
 public class Zadanie_5_8 {
 
-//    public static int liczbaSłów = 0;
 
     public static int wordCount(String text) {
 
-        int iloscSlow = 0;
+        // usunalem delimeter, bo defaultowo StringTokenizer juz uzywa wszystkich bialych znakow
+        StringTokenizer tabelaZeSlowami = new StringTokenizer(text);
 
-        StringTokenizer tabelaZeSlowami = new StringTokenizer(text, " ");
-
-        while (tabelaZeSlowami.hasMoreTokens()) {
-            // dodałęm (String a) bo jak robiłem // z tego sout nizej to program sie zapętlał
-            // nie wiem jak znalezc dlugosc tej tabeli wiec zliczalem iloscSlow++, ale samo
-            // iloscSlow zawieszala program, musial bys sout lub ten (String a)
-            String a = tabelaZeSlowami.nextToken();
-//            System.out.println(tabelaZeSlowami.nextToken());
-            iloscSlow++;
-        }
-        return iloscSlow;
+        // StringSokenizer ma juz metode, ktora zlicza slowa :)
+        return tabelaZeSlowami.countTokens();
     }
 
     //Probowalem zliczac slowa przez petle fori albo String.split ale problem wystepowal albo przy kilku spacjach
     // (ktore zliczal), albo przy spacji na koncu lub poczatku.
     //Ale znalazlem w sieci StringTokenizer i wyniki sa OK ale nie bardzo rozumiem jak to dziala :), szczegolnie przy petli WHILE
     // dlaczego z sout dziala a bez tego nie chce, przeciez to tylko wydruk ?
+
+    /*
+    while (tabelaZeSlowami.hasMoreTokens()) {
+        tabelaZeSlowami.nextToken();
+        iloscSlow++;
+    }
+    return iloscSlow;
+
+    Popatrzmy jak dziala petla while
+    za kazdym przejsciem sprawdza czy wartosc w nawiasach jest true
+    jesli tak, to wykonuje blok jeszcze raz
+
+    Jak sprawdza to wywoluje metode `tabelaZeSlowami.hasMoreTokens()`
+    W ciele mowisz: "Ej, StringTokenizer, wez pobierz nastepny token"
+    Potem cialo sie konczy i wraca znowu do sprawdzenia warunku
+    Teraz token juz juz inny, albo juz nie ma kolejnych
+     */
 
 
     public static void main(String[] args) {
--- src/_002/Zadania_5/Zadanie_5_8Test.java
+++ src/_002/Zadania_5/Zadanie_5_8Test.java
@@ -6,17 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Zadanie_5_8Test {
 
-    Zadanie_5_8 obiekt;
-
     @Test
     void wordCount() {
 
 //        dobra wartosc do sprawdzenia
-        assertEquals(3, obiekt.wordCount("Ala ma kota"));
-        assertEquals(3, obiekt.wordCount("     Ala    ma    kota      "));
+        assertEquals(3, Zadanie_5_8.wordCount("Ala ma kota"));
+        assertEquals(3, Zadanie_5_8.wordCount("     Ala    ma    kota      "));
+
+        // tu ponizej znak "\n" to jest znak reprezentujacy nowa linie
+        // Biale znaki to:
+        // \n -> nowa linia
+        // \t -> tab
+        //   -> spacja
+        // \r -> microsoft w Windowsie dodaje to przed \n zeby tak reprezentowac nowa linie
+        assertEquals(3, Zadanie_5_8.wordCount("Ala \n ma kota"));
 
         //        zle wartosci do sprawdzenia
-        assertEquals(0, obiekt.wordCount("Ala ma kota"));
+        assertNotEquals(0, Zadanie_5_8.wordCount("Ala ma kota"));
 
     }
 }
