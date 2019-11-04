a29e5ffec196f4ece474499423288f815b1394e2
Tom <4308@tenbit.pl>
2019-11-04 18:19:26 +0100

    Zadanie 5.10 - dodalem tresc zadania na poczatku

--- src/_002/Zadania_5/Zadanie_5_10.java
+++ src/_002/Zadania_5/Zadanie_5_10.java
@@ -1,3 +1,14 @@
+//        Zdefiniować funkcję int strFindAndCount(String gdzie, String co) , która
+//        zlicza wystąpienia łańcucha co w łańcuchu gdzie . Jej wynikiem jest wyznaczona
+//        liczba wystąpień. Jeżeli nie udało się znaleźć łańcucha, to wtedy wynikiem
+//        jest, oczywiście, 0.
+//        Przykłady:
+//        strFindAndCount("Ala ma kota", "ma") - wynik to 1
+//        strFindAndCount("mama ma kota", "ma") - wynik to 3
+//        strFindAndCount("Ala mmaa ma kota", "ma") - wynik to 2
+//        strFindAndCount("Ala ma kota", "Asia") - wynik to 0
+
+
 package _002.Zadania_5;
 
 import utils.InputUtils;
